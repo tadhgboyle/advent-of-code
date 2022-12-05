1693f018b8b741bd7092aafa18e76e8095347114
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-12-05 15:20:09 -0700

    day 5

---
+++ src/year2022/day05.rb
@@ -0,0 +1,83 @@
+module Year2022
+  class Day05
+    def part1(input)
+      rows = []
+      commands = []
+      input.each_line do |line|
+        if line == ""
+          next
+        end
+        if line.start_with?("move")
+          commands << line.split(" ").map { |e| e.to_i }.select { |i| i > 0 }
+        else
+          row = line.chomp.split("").map do |e|
+            if e == "[" || e == "]"
+              " "
+            else
+              e
+            end
+          end
+          rows << row.drop(1) if row != []
+        end
+      end
+
+      stacks = {}
+      rows.last.select { |e| e != " " }.each do |index|
+        stacks[index.to_i] = []
+      end
+      rows.pop
+
+      rows = rows.transpose.reject { |row| row.all? { |e| e == " " } }
+      rows.each_with_index do |row, index|
+        stacks[index + 1] = row.select { |e| e != " " }
+      end
+
+      commands.each do |amount, from, to|
+        elems = stacks[from].shift(amount)
+        stacks[to].unshift(*elems.reverse)
+      end
+
+      stacks.map { |s| s[1].first }.join
+    end
+
+    def part2(input)
+      rows = []
+      commands = []
+      input.each_line do |line|
+        if line == ""
+          next
+        end
+        if line.start_with?("move")
+          commands << line.split(" ").map { |e| e.to_i }.select { |i| i > 0 }
+        else
+          row = line.chomp.split("").map do |e|
+            if e == "[" || e == "]"
+              " "
+            else
+              e
+            end
+          end
+          rows << row.drop(1) if row != []
+        end
+      end
+
+      stacks = {}
+      rows.last.select { |e| e != " " }.each do |index|
+        stacks[index.to_i] = []
+      end
+      rows.pop
+
+      rows = rows.transpose.reject { |row| row.all? { |e| e == " " } }
+      rows.each_with_index do |row, index|
+        stacks[index + 1] = row.select { |e| e != " " }
+      end
+
+      commands.each do |amount, from, to|
+        elems = stacks[from].shift(amount)
+        stacks[to].unshift(*elems)
+      end
+
+      stacks.map { |s| s[1].first }.join
+    end
+  end
+end
