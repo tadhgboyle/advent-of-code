cb5af2a662c5d7fe6a4bf75416ce7c19bb56a1bd
Tadhg Boyle <tadhgsmboyle@gmail.com>
2023-12-02 14:18:32 -0800

    2023 day 2 part 1

--- src/year2023/day02.rb
+++ src/year2023/day02.rb
@@ -1,10 +1,39 @@
 module Year2023
   class Day02
-    PART1_ANSWER = ""
+    PART1_ANSWER = 2771 # 8 test
     PART2_ANSWER = ""
 
+    MAX_CUBES = {
+      "red" => 12,
+      "green" => 13,
+      "blue" => 14,
+    }.freeze
+
     def part1(input)
-      nil
+      possible_game_ids = []
+      input.each_line do |line|
+        line.chomp!
+        id = line.split(" ")[1].chop.to_i
+        sets = line.split(": ").drop(1).join.split("; ")
+        possible = true
+        sets.each do |set|
+          cubes_by_colour = {}
+          cubes = set.split(", ")
+          cubes.each do |cube|
+            cube = cube.split(" ")
+            count = cube.first.to_i
+            colour = cube.last
+            cubes_by_colour[colour] ||= 0
+            cubes_by_colour[colour] += count
+          end
+          possible = false if cubes_by_colour.any? do |colour, count|
+            count > MAX_CUBES[colour]
+          end
+        end
+        possible_game_ids << id if possible
+      end
+
+      possible_game_ids.sum
     end
 
     def part2(input)
