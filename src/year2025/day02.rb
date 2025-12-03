0064f9c7cc4c41fbeddeb9fd84ce74d13e3a4793
tadhgboyle <tadhgsmboyle@gmail.com>
2025-12-02 20:30:29 -0800

    2025 day 2 part 2

--- src/year2025/day02.rb
+++ src/year2025/day02.rb
@@ -1,7 +1,7 @@
 module Year2025
   class Day02
-    PART1_ANSWER = ""
-    PART2_ANSWER = ""
+    PART1_ANSWER = 26255179562
+    PART2_ANSWER = 31680313976
 
     def part1(input)
       ranges = input.split(",").map do |range|
@@ -28,7 +28,27 @@ module Year2025
     end
 
     def part2(input)
-      nil
+      ranges = input.split(",").map do |range|
+        start_n, end_n = range.split("-").map(&:to_i)
+        (start_n..end_n)
+      end
+
+      invalid_ids = []
+
+      ranges.each do |range|
+        range.each do |id|
+          id = id.to_s
+          chunk_sizes_to_check = (1..id.length - 1)
+          chunk_sizes_to_check.each do |chunk_size|
+            if id.split("").each_slice(chunk_size).uniq.size == 1
+              invalid_ids << id.to_i
+              break
+            end
+          end
+        end
+      end
+
+      invalid_ids.sum
     end
   end
 end
