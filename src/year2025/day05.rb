3a9418727ad145b07e93245fafbe98d74105c691
tadhgboyle <tadhgsmboyle@gmail.com>
2025-12-05 16:48:56 -0800

    2025 day 5 part 2

--- src/year2025/day05.rb
+++ src/year2025/day05.rb
@@ -1,9 +1,10 @@
 require "pry"
+require "set"
 
 module Year2025
   class Day05
     PART1_ANSWER = 868
-    PART2_ANSWER = ""
+    PART2_ANSWER = 354143734113772
 
     def part1(input)
       fresh_ranges = []
@@ -11,8 +12,8 @@ module Year2025
       input.chomp.split("\n").each do |line|
         break if line == ""
 
-        ranged = line.split("-")
-        fresh_ranges << (ranged[0].to_i..ranged[1].to_i)
+        ranged = line.split("-").map(&:to_i)
+        fresh_ranges << (ranged[0]..ranged[1])
       end
 
       fresh_ingredients = 0
@@ -30,7 +31,29 @@ module Year2025
     end
 
     def part2(input)
-      nil
+      possible_fresh_ingredients = Set.new
+
+      input.chomp.split("\n").each do |line|
+        break if line == ""
+
+        ranged = line.split("-").map(&:to_i)
+        ranged[0].upto(ranged[1]).each do |i|
+          possible_fresh_ingredients << i
+        end
+      end
+
+      possible_fresh_ingredients.size
+
+      # ranges = []
+
+      # input.chomp.split("\n").each do |line|
+      #   break if line == ""
+
+      #   ranged = line.split("-").map(&:to_i)
+      #   ranges << (ranged[0]..ranged[1]).to_a
+      # end
+
+      # ranges.flatten.uniq.size
     end
   end
 end
